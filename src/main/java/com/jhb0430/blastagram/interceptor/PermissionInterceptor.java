081ec517d79fb778d493a55018a03e698a3d23f7
jhb0430 <leky2224@gmail.com>
2025-01-08 17:31:16 +0900

    [배지현] -PermissionInterceptor.java

--- src/main/java/com/jhb0430/blastagram/interceptor/PermissionInterceptor.java
+++ src/main/java/com/jhb0430/blastagram/interceptor/PermissionInterceptor.java
@@ -22,11 +22,33 @@ public class PermissionInterceptor implements HandlerInterceptor{
 		
 		String uri = request.getRequestURI();
 		
-		if(userId == null) {
-			// ???아니... 반대로 생각하자....
-			// //login-view sign-up-view가 아니면 아~~ 큰일났다 
-			if(uri.startsWith("/");
-		}
+		// 로그인이 안된 상태에서 메모와 관련된 기능 페이지의 접근을 막는다 
+				// 로그인 페이지로 이동
+				if(userId == null) {
+					// /post 로 시작하는 url path 확인
+					if(uri.startsWith("/login-view")) {
+						
+						// 로그인 페이지로 리다이렉트 정보를 response에 담는다. 
+						response.sendRedirect("/user/login-view");
+						return false;
+					}
+				} else {
+					// 로그인이 되어 있는 경우 사용자와 관련된 페이지 접근을 막는다
+					// 메모 리스트 페이지로 이동
+					// /user 로 시작하는 url path 확인
+					if(uri.startsWith("/user")) {
+						response.sendRedirect("/post/list-view");
+						return false;
+					}
+				}
+				
+				return true;
+		
+//		if(userId == null) {
+//			// ???아니... 반대로 생각하자....
+//			// //login-view sign-up-view가 아니면 아~~ 큰일났다 
+//			
+//		}
 		
 	}
 	
